6835b86e06cbabef46404bbcb6a013d615210ed2
aliyermekkbtu <a_yermek@kbtu.kz>
2024-09-26 13:37:56 +0500

    3

---
+++ .vscode/3lab/classes/1.py
@@ -0,0 +1,16 @@
+class MyClass():
+    def __init__(self):
+        self.str = ""
+
+    def getString(self):
+        self.str = input("Enter a string: ") 
+    def printString(self):
+        print(self.str.upper()) 
+
+a = MyClass()
+
+
+a.getString()
+
+
+a.printString()
