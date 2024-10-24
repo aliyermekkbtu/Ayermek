f08494adcae1c6266f3f1d029d977d3b1cdf3e63
aliyermekkbtu <a_yermek@kbtu.kz>
2024-10-24 12:34:33 +0500

    Delete 10.py

--- 10.py
+++
@@ -1,8 +0,0 @@
-import re
-def camel_to_snake(s):
-    pattern = r'([a-z])([A-Z])'
-    return re.sub(pattern, r'\1_\2', s).lower()
-
-# Test cases
-print(camel_to_snake('camelCaseString'))      # 'camel_case_string'
-print(camel_to_snake('thisIsATest'))          # 'this_is_a_test'
