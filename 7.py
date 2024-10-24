6e33710759c24b442a7ca17387e3c2f536e1e890
aliyermekkbtu <a_yermek@kbtu.kz>
2024-10-24 12:35:52 +0500

    Delete 7.py

--- 7.py
+++
@@ -1,8 +0,0 @@
-import re
-def snake_to_camel(s):
-    components = s.split('_')
-    return components[0] + ''.join(x.title() for x in components[1:])
-
-# Test cases
-print(snake_to_camel('snake_case_string'))    # 'snakeCaseString'
-print(snake_to_camel('this_is_a_test'))       # 'thisIsATest'
