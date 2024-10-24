b0a4e6c187a9f71299b39a1da4b46a1bd8e77bb8
aliyermekkbtu <a_yermek@kbtu.kz>
2024-10-24 12:35:43 +0500

    Delete 6.py

--- 6.py
+++
@@ -1,8 +0,0 @@
-import re
-def replace_with_colon(s):
-    pattern = r'[ ,.]'
-    return re.sub(pattern, ':', s)
-
-# Test cases
-print(replace_with_colon('Hello, world. How are you?'))  # 'Hello:world:How:are:you?'
-
